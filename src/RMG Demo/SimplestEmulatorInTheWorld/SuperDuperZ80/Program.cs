5d137f970192c73ebda3efc099a1555ff345085a
Neil Hewitt <neil.hewitt@gmail.com>
2022-12-06 17:46:20 +0000

    Simplest Z80 emulation ever!

---
+++ src/RMG Demo/SimplestEmulatorInTheWorld/SuperDuperZ80/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SuperDuperZ80
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating Z80...");
+            Z80 z80 = new Z80();
+            z80.OnAfterInstruction += Z80_OnAfterInstruction;
+
+            z80.Poke(0, 0x3C);
+            z80.Poke(1, 0xC3);
+
+            Console.WriteLine($"Ready: \t A: {z80.A} \t PC: {z80.PC}");
+            Console.ReadKey(false);
+
+            z80.Run();
+        }
+
+        private static void Z80_OnAfterInstruction(object sender, string instruction)
+        {
+            Z80 z80 = (Z80)sender;
+            Console.WriteLine($"Instruction: {instruction} \t A: {z80.A} \t PC: {z80.PC}");
+            Console.ReadKey(false);
+        }
+    }
+}
