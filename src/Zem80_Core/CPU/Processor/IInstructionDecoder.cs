378576828489ce15829390ee9569a780618f3754
Neil Hewitt <neil.hewitt@gmail.com>
2025-09-20 19:12:51 +0100

    Change course - remove IInstructionDecoder, make InstructionDecoder static.

--- src/Zem80_Core/CPU/Processor/IInstructionDecoder.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Zem80.Core.CPU
-{
-    public interface IInstructionDecoder
-    {
-        InstructionPackage DecodeInstruction(byte[] instructionBytes, ushort address, out bool skipNextByte, out bool opcodeErrorNOP);
-    }
-}
