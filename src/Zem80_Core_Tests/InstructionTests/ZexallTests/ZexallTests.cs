5e39322fb53b56e8bebf9616b3ca6a4d5f954e2d
Neil Hewitt <neil.hewitt@gmail.com>
2025-09-24 16:14:17 +0100

    Remove option to run ZexDoc tests vs ZexAll.

--- src/Zem80_Core_Tests/InstructionTests/ZexallTests/ZexallTests.cs
+++ src/Zem80_Core_Tests/InstructionTests/ZexallTests/ZexallTests.cs
@@ -17,16 +17,11 @@ namespace Zem80.Core.Tests.Zexall
     {
         private Processor _cpu;
         private TestRunner _runner;
-        private bool _runZexall;
-        private bool _runZexdoc;
 
         [OneTimeSetUp]
         public void Setup()
         {
             _cpu = new Processor();
-
-            _runZexall = true;
-            _runZexdoc = false;
 
             // set up the ZexNext test runner using the zipped test file (zexall.zip)
             Task.Run(() => _runner = new TestRunner(
@@ -100,8 +95,6 @@ namespace Zem80.Core.Tests.Zexall
         [TestCase("ld (<bc,de>),a")]
         public void Zexall(string testSetName)
         {
-            if (!_runZexall) return;
-
             // this is basically ZexNext tests running inside NUnit tests as a host
 
             // ensure the Visual Studio UI remains responsive while the tests are loading and compiling
@@ -112,82 +105,6 @@ namespace Zem80.Core.Tests.Zexall
             Assert.That(results.All(x => x.Passed));
         }
 
-        [Test]
-        [TestCase("<adc,sbc> hl,<bc,de,hl,sp>")]
-        [TestCase("add hl,<bc,de,hl,sp>")]
-        [TestCase("add ix,<bc,de,ix,sp>")]
-        [TestCase("add iy,<bc,de,iy,sp>")]
-        [TestCase("aluop a,nn")]
-        [TestCase("aluop a,<b,c,d,e,h,l,(hl),a>")]
-        [TestCase("aluop a,<ixh,ixl,iyh,iyl>")]
-        [TestCase("aluop a,(<ix,iy>+1)")]
-        [TestCase("bit n,(<ix,iy>+1)")]
-        [TestCase("bit n,<b,c,d,e,h,l,(hl),a>")]
-        [TestCase("cpd<r>")]
-        [TestCase("cpi<r>")]
-        [TestCase("<daa,cpl,scf,ccf>")]
-        [TestCase("<inc,dec> a")]
-        [TestCase("<inc,dec> b")]
-        [TestCase("<inc,dec> bc")]
-        [TestCase("<inc,dec> c")]
-        [TestCase("<inc,dec> d")]
-        [TestCase("<inc,dec> de")]
-        [TestCase("<inc, dec> e")]
-        [TestCase("<inc, dec> h")]
-        [TestCase("<inc,dec> hl")]
-        [TestCase("<inc,dec> ix")]
-        [TestCase("<inc,dec> iy")]
-        [TestCase("<inc,dec> l")]
-        [TestCase("<inc,dec> (hl)")]
-        [TestCase("<inc,dec> sp")]
-        [TestCase("<inc,dec> (<ix,iy>+1)")]
-        [TestCase("<inc,dec> ixh")]
-        [TestCase("<inc,dec> ixl")]
-        [TestCase("ld <bc,de>,(nnnn)")]
-        [TestCase("ld hl,(nnnn)")]
-        [TestCase("ld sp,(nnnn)")]
-        [TestCase("ld <ix,iy>,(nnnn)")]
-        [TestCase("ld (nnnn),<bd,de>")]
-        [TestCase("ld (nnnn),hl")]
-        [TestCase("ld (nnnn),sp")]
-        [TestCase("ld (nnnn),<ix,iy>")]
-        [TestCase("ld <bc,de,hl,sp>,nnnn")]
-        [TestCase("ld a,<(bc),(de)>")]
-        [TestCase("ld <b,c,d,e,h,l,(hl),a>,nn")]
-        [TestCase("ld (<ix,iy+1),nn")]
-        [TestCase("ld <b,c,d,e>,(<ix,iy>+1)")]
-        [TestCase("ld <h,l>,(<ix,iy>+1)")]
-        [TestCase("ld a,(<ix,iy>+1)")]
-        [TestCase("ld <ixh,ixl,iyh,iyl>,nn")]
-        [TestCase("ld <b,c,d,e,h,l,a>,<b,c,d,e,h,l,a>")]
-        [TestCase("ld <b,c,d,e,ixy,a>,<b,c,d,e,ixy,a>")]
-        [TestCase("ld a,(nnnn)")]
-        [TestCase("ldd<r>")]
-        [TestCase("ldi<r>")]
-        [TestCase("neg")]
-        [TestCase("<rrd,rld>")]
-        [TestCase("<rlca,rrca,rla,rra>")]
-        [TestCase("shf/rot (<ix,iy>+1)")]
-        [TestCase("shf/rot <b,c,d,e,h,l,(hl),a>")]
-        [TestCase("<set,res> n,<bcdehl(hl)a>")]
-        [TestCase("<set,res> n,(<ix,iy>+1)")]
-        [TestCase("ld (<ix,iy>+1),<b,c,d,e>")]
-        [TestCase("ld (<ix,iy>+1),<h,l>")]
-        [TestCase("ld (<ix,iy>+1),a")]
-        [TestCase("ld (<bc,de>),a")]
-        public void Zexdoc(string testSetName)
-        {
-            if (!_runZexdoc) return;
-
-            // this is basically ZexNext tests running inside NUnit tests as a host
-
-            // ensure the Visual Studio UI remains responsive while the tests are loading and compiling
-            while (_runner == null) Thread.Sleep(0);
-
-            // run the specified test set
-            IEnumerable<TestResult> results = _runner.Run(testSetName, ExecuteTestCycle, true);
-            Assert.That(results.All(x => x.Passed));
-        }
 
         private TestState ExecuteTestCycle(TestState input)
         {
