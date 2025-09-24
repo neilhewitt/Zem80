23cc17640cce8febb3f4d1109dcaa304077529c7
Neil Hewitt <neil.hewitt@gmail.com>
2025-09-24 15:08:46 +0100

    Incorporate ZexNext framework into Zem80 project.

---
+++ src/ZexNext_Core/TestResult.cs
@@ -0,0 +1,29 @@
+﻿namespace ZexNext.Core
+{
+    public class TestResult
+    {
+        public TestCycle TestCycle { get; private set; }
+        public string TestName { get; private set; }
+        public string Mnemonic { get; private set; }
+        public bool Passed { get; private set; }
+        public TestState InitialState { get; private set; }
+        public TestState ExpectedState { get; private set; }
+        public TestState ActualState { get; private set; }
+
+        public override string ToString()
+        {
+            return (Passed ? "PASSED " : "FAILED") + "\nInitial state: " + InitialState.ToString() + "\nExpected state: " + ExpectedState.ToString() + "\nActual state: " + ActualState.ToString();
+        }
+
+        public TestResult(TestCycle testCycle, string testName, string mnemonic, bool passed, TestState initial, TestState expected, TestState actual)
+        {
+            TestCycle = testCycle;
+            TestName = testName;
+            Mnemonic = mnemonic;
+            Passed = passed;
+            InitialState = initial;
+            ExpectedState = expected;
+            ActualState = actual;
+        }
+    }
+}
---
+++ src/ZexNext_Runner/readme.md
@@ -0,0 +1,3 @@
+﻿The data file, zexdoc.zxl, is not committed here as it's too big for GitHub.
+
+Instructions on how to get it will be added here at a later stage!
