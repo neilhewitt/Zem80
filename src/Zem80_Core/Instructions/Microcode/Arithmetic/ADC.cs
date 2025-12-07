477f25c62169499baa7095c7777d53f38c6d265b
Neil Hewitt <neil.hewitt@gmail.com>
2025-12-07 14:28:46 +0000

    Mid-way check in to allow testing (NOT COMPLETE)

--- src/Zem80_Core/Instructions/Microcode/Arithmetic/ADC.cs
+++ src/Zem80_Core/Instructions/Microcode/Arithmetic/ADC.cs
@@ -43,4 +43,47 @@ namespace Zem80.Core.CPU
         {
         }
     }
+
+    public class ADD_ADC_SUB_SBC : MicrocodeBase, IMicrocode
+    {
+        private bool _withCarry;
+        private bool _subtracts;
+
+        public ExecutionResult Execute(Processor cpu, InstructionPackage package, Action<ExecutionState> onMachineCycle)
+        {
+            Instruction instruction = package.Instruction;
+            InstructionData data = package.Data;
+            Flags flags = cpu.Flags.Clone();
+            IRegisters r = cpu.Registers;
+
+            if (instruction.TargetsWordRegister)
+            {
+                // it's one of the 16-bit adds / sbc (HL,DE etc)
+                // ADC, SBC only allow HL. Add allows BC, DE, HL, SP
+
+                WordRegister destination = instruction.Target.AsWordRegister();
+                ushort left = r[destination];
+                ushort right = GetSourceWordAndAddTimingAndEvents();
+                (r.HL, flags) = Arithmetic.Add(left, right, flags.Carry, true, flags);
+                r.WZ = (ushort)(left + 1);
+            }
+            else
+            {
+                // it's one of the 8-bit adds / subs
+
+                byte left = r.A;
+                if (instruction.IsIndexed) cpu.Timing.InternalOperationCycle(5);
+                byte right = Resolver.GetSourceByte(instruction, data, cpu, 3);
+                (r.A, flags) = Arithmetic.Add(left, right, flags.Carry);
+            }
+
+            return new ExecutionResult(package, flags);
+        }
+
+        public ADD_ADC_SUB_SBC(string z80Mnemonic)
+        {
+            _withCarry = z80Mnemonic == "ADC" || z80Mnemonic == "SBC";
+            _subtracts = z80Mnemonic == "SUB" || z80Mnemonic == "SBC";
+        }
+    }
 }
--- src/Zem80_Core/Instructions/Microcode/MicrocodeBase.cs
+++ src/Zem80_Core/Instructions/Microcode/MicrocodeBase.cs
@@ -21,7 +21,147 @@ namespace Zem80.Core.CPU
             _onMachineCycle = onMachineCycle;
 
             MachineCycle = package.Instruction.MachineCycles;
-            _machineCycles = new List<MachineCycle>(MachineCycle.Cycles); // want a realised countable list
+            //_machineCycles = new List<MachineCycle>(MachineCycle.Cycles); // want a realised countable list
+        }
+
+        public byte GetSourceByteAndAddTimingAndEvents()
+        {
+            // this fetches a byte operand value for the instruction given, adjusting how it is fetched based on the addressing of the instruction
+
+            IRegisters r = _cpu.Registers;
+            Instruction instruction = _package.Instruction;
+            InstructionData data = _package.Data;
+            ushort address = 0x0000;
+
+            byte value;
+            ByteRegister source = instruction.Source.AsByteRegister();
+            if (source != ByteRegister.None)
+            {
+                // operand comes from another byte register directly (eg LD A,B)
+                // no additional timing needed
+                value = r[source];
+            }
+            else
+            {
+                if (instruction.Argument1 == InstructionElement.ByteValue)
+                {
+                    // operand is supplied as an argument (eg LD A,n)
+                    // timing and events are already handled at decode time
+                    value = data.Argument1;
+                }
+                else if (instruction.Source == InstructionElement.None)
+                {
+                    // operand is fetched from a memory location but the source and target are the same (eg INC (HL) or INC (IX+d))
+                    address = instruction.Target.AsWordRegister() switch
+                    {
+                        WordRegister.IX => (ushort)(r.IX + (sbyte)data.Argument1),
+                        WordRegister.IY => (ushort)(r.IY + (sbyte)data.Argument1),
+                        _ => r.HL
+                    };
+
+                    value = _cpu.Memory.ReadByteAt(address);
+                    _cpu.Timing.MemoryReadCycle(address, value, MachineCycle.MemoryRead.TStates);
+                    NotifyMachineCycle(MachineCycle.MemoryRead, value);
+                }
+                else
+                {
+                    // operand is fetched from a memory location and assigned elsewhere (eg LD A,(HL) or LD B,(IX+d))
+                    address = instruction.Source.AsWordRegister() switch
+                    {
+                        WordRegister.IX => (ushort)(r.IX + (sbyte)data.Argument1),
+                        WordRegister.IY => (ushort)(r.IY + (sbyte)data.Argument1),
+                        _ => r.HL
+                    };
+
+                    // indexed addressing adds an internal operation cycle
+                    if (instruction.IsIndexed)
+                    {
+                        _cpu.Timing.InternalOperationCycle(MachineCycle.InternalOperation1.TStates);
+                        NotifyMachineCycle(MachineCycle.InternalOperation1);
+                    }
+
+                    // normal timing
+                    value = _cpu.Memory.ReadByteAt(address);
+                    _cpu.Timing.MemoryReadCycle(address, value, MachineCycle.MemoryRead.TStates);
+                    NotifyMachineCycle(MachineCycle.MemoryRead, value);
+                }
+            }
+
+            return value;
+        }
+
+        public ushort GetSourceWordAndAddTimingAndEvents()
+        {
+            IRegisters r = _cpu.Registers;
+            Instruction instruction = _package.Instruction;
+            InstructionData data = _package.Data;
+            ushort address = 0x0000;
+
+            ushort value;
+            WordRegister source = instruction.Source.AsWordRegister();
+            if (source != WordRegister.None)
+            {
+                // source is word register
+                value = r[source];
+                NotifyMachineCycle(MachineCycle.InternalOperation1);
+                NotifyMachineCycle(MachineCycle.InternalOperation2);
+            }
+
+            else
+            {
+                if (instruction.Argument1 == InstructionElement.ByteValue && instruction.Argument2 == InstructionElement.ByteValue)
+                {
+                    value = data.ArgumentsAsWord;
+                    NotifyMachineCycle(MachineCycle.OperandReadLow);
+                    NotifyMachineCycle(MachineCycle.OperandReadHigh);
+                }
+                else
+                {
+                    address = instruction.Source.AsWordRegister() switch
+                    {
+                        WordRegister.IX => (ushort)(r.IX + (sbyte)data.Argument1),
+                        WordRegister.IY => (ushort)(r.IY + (sbyte)data.Argument1),
+                        _ => r.HL
+                    };
+
+                    // indexed addressing adds an internal operation cycle
+                    if (instruction.IsIndexed)
+                    {
+                        _cpu.Timing.InternalOperationCycle(MachineCycle.InternalOperation1.TStates);
+                        NotifyMachineCycle(MachineCycle.InternalOperation1);
+                    }
+
+                    value = _cpu.Memory.ReadWordAt(address);
+
+
+                    _cpu.Timing.MemoryReadCycle(address, value.LowByte(), MachineCycle.OperandReadLow.TStates);
+                    NotifyMachineCycle(MachineCycle.OperandReadLow, value.LowByte());
+                    _cpu.Timing.MemoryReadCycle(address, value.HighByte(), MachineCycle.OperandReadHigh.TStates);
+                    NotifyMachineCycle(MachineCycle.OperandReadHigh, value.HighByte());
+                }
+            }
+
+            return value;
+        }
+
+        public void InternalOperation1()
+        {
+            MachineCycle cycle = MachineCycle.InternalOperation1;
+            if (cycle != null)
+            {
+                _cpu.Timing.InternalOperationCycle(cycle.TStates);
+                NotifyMachineCycle(cycle);
+            }
+        }
+
+        public void InternalOperation2()
+        {
+            MachineCycle cycle = MachineCycle.InternalOperation2;
+            if (cycle != null)
+            {
+                _cpu.Timing.InternalOperationCycle(cycle.TStates);
+                NotifyMachineCycle(cycle);
+            }
         }
 
         public void NotifyMachineCycle(MachineCycle cycle, byte? arg1 = null, byte? arg2 = null)
